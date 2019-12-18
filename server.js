83964fa5d1236488727d03f09deb7bc0420a5e35
Glitch (hello-express) <none>
2019-12-18 04:10:22 +0000

    🚏🚛 Checkpoint ./server.js:1844607/439

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const CH = new CommandHandler({
     prefix: config.prefix 
 });
 
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT); 
+setInterval(() => {
+  http.get(`http://xenonbot22.glitch.me/`);
+}, 280000);
+
 
 client.on("ready", () => {
     console.log("Ready !");
