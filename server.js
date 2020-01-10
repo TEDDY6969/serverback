d5bf77b69891498a2db02d243506eca5a8326ab3
Glitch (hello-express) <none>
2020-01-10 14:09:33 +0000

    🗺⚡️ Checkpoint ./server.js:1844607/2500

--- server.js
+++ server.js
@@ -26,65 +26,7 @@ client.on("ready", () => {
     client.user.setActivity(`${approx(145000)} Guilds | x!help`, {type: "WATCHING"})
 });
 
-const fetch = require('node-fetch');
- 
-const qs = require('querystring');//Toxic COdes
- 
-let prefix = "x!";
-client.on('message', async message => { //Toxic COdes
-    let alias = message.content.split(" ")[0].substring(prefix.length);
-    let args = message.content.split(" ").slice(1);
- 
-    if (alias == 'docs') {
-        if (!args[0]) return;
-        const query = args.join(" "); //Toxic COdes//Toxic COdes
-        const queryString = qs.stringify({  //Toxic COdes
-            q: query  
-        });
-      await fetch(`https://djsdocs.sorta.moe/v2/embed`, {
-            params: { src: 'stable', q: query }
-        })
-            .then(res => res.json())
-            .then(async res => {
-                try {
-                    let i = new RichEmbed();
-                    let fields = res.fields;
-                    let properties = fields.filter(r => r.name == 'Properties');
-                    let methods = fields.filter(r => r.name == 'Methods');
-                    let events = fields.filter(r => r.name == 'Events');
-                    let params = fields.filter(r => r.name == 'Params');
-                    let returns = fields.filter(r => r.name == 'Returns');
-                    let examples = fields.filter(r => r.name == 'Examples');
-                    i.setColor(res.color);
-                    i.setTitle(res.author.name);
-                    i.setURL(res.url);
-                    i.setDescription(res.description);
-                    i.setThumbnail(res.author.icon_url);
-                    if (properties.length != 0) {
-                        i.addField('- Properties', properties[0].value);
-                    }
-                    if (methods.length != 0) {
-                        i.addField('- Methods', methods[0].value);
-                    }
-                    if (events.length != 0) {
-                        i.addField('- Events', events[0].value);
-                    }
-                    if (params.length != 0) {
-                        i.addField('- Params', params[0].value);
-                    }
-                    if (returns.length != 0) {
-                        i.addField('- Returns', returns[0].value);
-                    }
-                    if (examples.length != 0) {
-                        i.addField('- Examples', examples[0].value);
-                    }
-                    await message.channel.send(i);
-                } catch (e) {
-                  throw e;
-                }
-            });
-    } //Toxic COdes
-}); //Toxic COdes
+
 
 client.on("message", async (message) => {
 
